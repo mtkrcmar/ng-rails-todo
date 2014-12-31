4b3f54fe3c7d05f2390e8e846579ca8cdffdcf99
Michael Krcmarik <michaelkrcmarik@Michaels-Mac-Pro.local>
2014-12-30 23:44:15 -0500

    api configuration

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :todos
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
